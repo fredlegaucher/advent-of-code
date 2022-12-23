d0f1e0e2243f0840e56c805d41a3363b86332c2a
Frederic Laurent <frederic.laurent@starlingbank.com>
2022-12-23 17:50:57 +0000

    Update day 22 tests

--- 2022/day-22-1/main_test.go
+++ 2022/day-22-1/main_test.go
@@ -11,11 +11,3 @@ func TestSolveExample(t *testing.T){
         t.Fatalf("Expected 6,8,0 not %v,%v,%v",a,b,c)
     }
 }
-
-// func TestSolveProblem(t *testing.T){
-// 	a,b,c := solveProblem("input.txt")	
-    
-//     if a != 8666 || b != 4363 || c!= -8805 {
-//         t.Fatalf("Expected 8666,4363,-8805 not %v,%v,%v",a,b,c)
-//     }
-// }
--- 2022/day-22-2/main_test.go
+++ 2022/day-22-2/main_test.go
@@ -4,18 +4,10 @@ import (
 	"testing"
 )
 
-func TestSolveExample(t *testing.T){
+func TestSolveProblem(t *testing.T){
 	a,b,c := solveProblem("input.txt")	
     
-    if a != 6 || b != 8 || c!= 0 {
-        t.Fatalf("Expected 6,8,0 not %v,%v,%v",a,b,c)
+    if a != 197 || b != 30 || c!= 2 {
+        t.Fatalf("Expected 197,30,2 not %v,%v,%v",a,b,c)
     }
 }
-
-// func TestSolveProblem(t *testing.T){
-// 	a,b,c := solveProblem("input.txt")	
-    
-//     if a != 8666 || b != 4363 || c!= -8805 {
-//         t.Fatalf("Expected 8666,4363,-8805 not %v,%v,%v",a,b,c)
-//     }
-// }
